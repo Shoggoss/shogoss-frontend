ed39c7d86abe0866daf4c0d471d9f1cba57e73e9
sozysozbot / hsjoihs <sozysozbot@gmail.com>
2022-11-21 02:35:52 +0900

    Add the banner

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,16 @@ function App() {
           Learn React
         </a>
       </header>
+      <div
+        style={{ position: "absolute", top: "550px", border: "1px solid black", backgroundColor: "#dfeed2", width: "900px", padding: "5px" }}>
+        <p>このサイトを実装した人は以下のボドゲも実装しています：</p>
+        <a target="_blank" rel="noopener noreferrer"
+          href="http://jurliyuuri.github.io/cerke_online_alpha/index.html"><img style={{ border: "1px solid black" }}
+            src="https://cdn.discordapp.com/attachments/668577426149867553/1034886204207943751/Capture_decran_2022-10-27_a_2.47.34_AM.png"
+            alt="机戦を遊ぶ" height="300" /></a>
+        <a target="_blank" rel="noopener noreferrer" href="http://keserima.com/play/index.html"><img
+          style={{ border: "1px solid black" }} src="http://keserima.com/imgs/keserima.png" height="300" alt="ケセリマを遊ぶ" /></a>
+      </div>
     </div>
   );
 }
